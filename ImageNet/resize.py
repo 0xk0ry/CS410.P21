fbb7c71d50d3de3aa5db6204032c069065211d50
kory <phamtruc.main@gmail.com>
2025-05-02 18:58:49 +0700

    Refactor path definitions in resize.py: change string paths to Path objects for consistency

--- ImageNet/resize.py
+++ ImageNet/resize.py
@@ -6,9 +6,9 @@ import multiprocessing
 cpus = multiprocessing.cpu_count()
 cpus = min(48,cpus)
 
-PATH = '/kaggle/input/tiny-imagenet/tiny-imagenet-200'
+PATH = Path('/kaggle/input/tiny-imagenet/tiny-imagenet-200')
 #DEST = Path('/mnt/ram')
-DEST = '/kaggle/working/data/ImageNet'
+DEST = Path('/kaggle/working/data/ImageNet')
 #szs = (int(128*1.25), int(256*1.25))
 # szs = (int(160*1.25),)
 szs = (160,352)
