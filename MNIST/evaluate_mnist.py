969cf0f05bf3c686c30272cb84062f441c76c5ba
kory <phamtruc.main@gmail.com>
2025-05-02 17:16:07 +0700

    Change logging level from DEBUG to INFO in evaluate_mnist.py

--- MNIST/evaluate_mnist.py
+++ MNIST/evaluate_mnist.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(
     format='[%(asctime)s %(filename)s %(name)s %(levelname)s] - %(message)s',
     datefmt='%Y/%m/%d %H:%M:%S',
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 
 def clamp(X, lower_limit, upper_limit):
