ced81185cd0412c9d21c45756f50d95747dbe5ba
kory <phamtruc.main@gmail.com>
2025-05-04 09:56:50 +0700

    Replace SGD optimizer with Adam for improved training performance

--- MNIST/train_mnist.py
+++ MNIST/train_mnist.py
@@ -48,7 +48,7 @@ def main():
     model = mnist_net().cuda()
     model.train()
 
-    opt = torch.optim.SGD(model.parameters(), lr=args.lr_max, momentum=0.9, weight_decay=5e-4)
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr_max)
     if args.lr_type == 'cyclic': 
         lr_schedule = lambda t: np.interp([t], [0, args.epochs * 2//5, args.epochs], [0, args.lr_max, 0])[0]
     elif args.lr_type == 'flat': 
